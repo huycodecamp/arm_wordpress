9d5a3494bf2138a2bdda345d77a430d2e6b326b1
Lâm <lam.hd.2210@aptech.learning.edu>
2023-10-15 03:22:40 +0700

    Trang details 80%

--- wp-content/themes/arm_layout/single-post.php
+++ wp-content/themes/arm_layout/single-post.php
@@ -1,134 +1,87 @@
 <?php
 /* Template Name: single */
 ?>
+
 <?php get_header() ?>
 
-<section id="main"> 
-        <div class="content-box">
-            <div class="container">
-                <div class="row justify-content-between">
-                    <div class="col-xl-7">
-                        <p class="content-box__text">Maintaining all the activities following the theme "Learn smart -
-                            Go global - Be a happy you" in the school year 2023 - 2024, Olympia strives for Olympians'
-                            invaluable experience, such as a variety of global activities, many international
-                            playgrounds for English, Science, Math,... and to give Olympians room to research and be
-                            creative at this happy school.</p>
-                        <img class="w-100 content-box__img" src="./assets/img/chu-de-nam-hoc-2324-2.jpg"
-                            alt="Chủ đề năm học">
-                        <p class="content-box__text">2023 - 2024 is the year Olympia celebrates its 20th anniversary,
-                            marking a long journey in developing bilingual programs that meet international
-                            accreditation standards. Two decades of pursuing the philosophy of respecting differences,
-                            Olympia teachers have been helping generations of students get ready for life and apply
-                            successfully for top domestic and international universities.</p>
-                        <h4 class="content-box__heading">Related post</h4>
-                        <ul class="list-unstyled content-box__relatedPosts">
-                            <li class="content-box__item">
-                                <a href="#" class="content-box__relatedPost-link">Olympians grade 10 of the
-                                    international diploma program debuts english musical "Les Misérables" on showcase
-                                    night<i class="fa-solid fa-caret-right"></i></a>
-                            </li>
-                            <li class="content-box__item">
-                                <a href="#" class="content-box__relatedPost-link">From deposition to the emotional
-                                    explosion during the showcase made by grade 10 & 11 students<i
-                                        class="fa-solid fa-caret-right"></i></a>
-                            </li>
-                            <li class="content-box__item">
-                                <a href="#" class="content-box__relatedPost-link">Congratulations to 16 outstanding
-                                    Fellows of OGFP<i class="fa-solid fa-caret-right"></i></a>
-                            </li>
-                        </ul>
+<section id="main">
+    <div class="content-box">
+        <div class="container" style="margin: 0 auto">
+            <div class="row justify-content-between">
+                <div class="col-xl-7">
+
+
+                    <div class="entry-content image-only-post">
+                        <!-- Nội dung bài viết -->
+                        <?php
+                        if (have_posts()) :
+                            while (have_posts()) : the_post();
+                                the_content();
+                            endwhile;
+                        endif;
+                        ?>
+
                     </div>
-                    <div class="col-xl-3">
-                        <h4 class="content-slidebar__heading text-uppercase">Featured news</h4>
-                        <ul class="list-unstyled">
-                            <li class="d-flex content-slidebar__item">
-                                <a href="#" class="content-slidebar-new-link">Olympians' experiences of studying in the
-                                    USA: "From an introvert person, I am confident to go to national wrestling
-                                    championship."</a>
-                                <p class="content-slidebar__new-date">09.06</p>
-                            </li>
-                            <li class="d-flex content-slidebar__item">
-                                <a href="#" class="content-slidebar-new-link">From deposition to the emotional explosion
-                                    during the showcase made by grade 10 & 11 students</a>
-                                <p class="content-slidebar__new-date">31.05</p>
-                            </li>
-                            <li class="d-flex content-slidebar__item">
-                                <a href="#" class="content-slidebar-new-link">[Viet Nam News] Students shine in charity
-                                    production of Les Miserables</a>
-                                <p class="content-slidebar__new-date">04.05</p>
-                            </li>
-                            <li class="d-flex content-slidebar__item">
-                                <a href="#" class="content-slidebar-new-link">The Olympia Schools and Portland public
-                                    school (USA) Establish cooperation ties</a>
-                                <p class="content-slidebar__new-date">17.04</p>
-                            </li>
-                            <li class="d-flex content-slidebar__item">
-                                <a href="#" class="content-slidebar-new-link">Letter to parents from our Head of
-                                    Schools</a>
-                                <p class="content-slidebar__new-date">31.08</p>
-                            </li>
-                            <li class="d-flex content-slidebar__item">
-                                <a href="#" class="content-slidebar-new-link">Becoming a Software Engineer: A rough and
-                                    promising journey</a>
-                                <p class="content-slidebar__new-date">30.08</p>
-                            </li>
-                            <li class="d-flex content-slidebar__item">
-                                <a href="#" class="text-uppercase content-slidebar-new-link">All news<i
-                                        class="fa-solid fa-caret-right"></i></a>
-                            </li>
-                        </ul>
-                        <div class="content-slidebar__level-wrapper">
-                            <h5 class="text-uppercase">School level</h5>
-                            <ul class="list-unstyled content-slidebar__level-lst">
-                                <li>
-                                    <a href="#">All</a>
-                                </li>
-                                <li>
-                                    <a href="#">Pre-primary school</a>
-                                </li>
-                                <li>
-                                    <a href="#">Primary school</a>
-                                </li>
-                                <li>
-                                    <a href="#">Middle school</a>
-                                </li>
-                                <li>
-                                    <a href="#">High school</a>
-                                </li>
-                                <li>
-                                    <a href="#">International diploma program</a>
-                                </li>
-                                <li>
-                                    <a href="#">IB program</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+
+
+
                 </div>
-            </div>
-            <div id="otherActivities">
-                <div class="container">
-                    <h5 class="otherActivities__heading">Other activities</h5>
-                    <ul class="list-unstyled otherActivities__lst row">
-                        <li class="col-xl-3">
-                            <div class="row">
-                                <div class="col-xl-12 col-4">
-                                    <a class="d-block" href="#">
-                                        <img class="w-100 otherActivities__item-img"
-                                            src="./assets/img/truongolympia-talkshow-careertalk02-2.png" alt="image">
-                                    </a>
-                                </div>
-                                <div class="col-xl-12 col-8">
-                                    <a class="otherActivities__item-link" href="#">
-                                        <p class="mb-0">[Career Talk #2] Law study in the global world</p>
-                                    </a>
-                                </div>
-                                <span class="otherActivities__item-date">18/09/2022</span>
-                            </div>
-                        </li>
+                <div class="col-xl-3">
+                    <h4 class="content-slidebar__heading text-uppercase">Tin Nổi Bật</h4>
+                    <ul class="list-unstyled">
+                        <?php
+                        $args = array(
+                            'post_type' => 'post',  // Loại bài viết (có thể là post, page, hoặc loại tùy chỉnh khác)
+                            'posts_per_page' => 6,  // Số bài viết bạn muốn hiển thị
+                            'orderby' => 'rand',    // Sắp xếp ngẫu nhiên
+                        );
+
+                        $query = new WP_Query($args);
+
+                        if ($query->have_posts()) {
+                            while ($query->have_posts()) {
+                                $query->the_post();
+                        ?>
+                                <li class="d-flex content-slidebar__item">
+                                    <a href="<?php the_permalink(); ?>" class="content-slidebar-new-link"><?php the_title(); ?></a>
+                                    <p class="content-slidebar__new-date"><?php the_date('d.m'); ?></p>
+                                </li>
+                        <?php
+                            }
+                            wp_reset_postdata(); // Đặt lại trạng thái của truy vấn
+                        }
+                        ?>
                     </ul>
+
+                    <div class="content-slidebar__level-wrapper">
+                        <h5 class="text-uppercase">School level</h5>
+                        <ul class="list-unstyled content-slidebar__level-lst">
+                            <li>
+                                <a href="#">All</a>
+                            </li>
+                            <li>
+                                <a href="#">Pre-primary school</a>
+                            </li>
+                            <li>
+                                <a href="#">Primary school</a>
+                            </li>
+                            <li>
+                                <a href="#">Middle school</a>
+                            </li>
+                            <li>
+                                <a href="#">High school</a>
+                            </li>
+                            <li>
+                                <a href="#">International diploma program</a>
+                            </li>
+                            <li>
+                                <a href="#">IB program</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
-    </section>
-    <?php get_footer() ?>
+    </div>
+</section>
+<?php get_footer() ?>
